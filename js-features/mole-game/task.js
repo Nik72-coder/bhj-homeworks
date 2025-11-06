15591cb039ad9754feabcdd8e6c2444aa3c591fe
Nikolay <Kreicer0@gmail.com>
2025-11-06 10:14:44 +0300

    ДЗ Игра «Убей кротов»

---
+++ js-features/mole-game/task.js
@@ -0,0 +1,16 @@
+
+const hitsWin = 10; // макс. количество попаданий
+const missesLose = 5; // макс. количество промахов
+const fieldHoles = 9; // поле 3х3
+
+let hits = 0; // текущее количество попаданий
+let misses = 0; // текущее количество промахов
+
+const deadsMole = document.getElementById('dead');  // Попадания
+const missesDisplay = document.getElementById('lost'); // Промахи
+const gameContainer = document.getElementById('hole-game'); // Поле
+
+function getHole( index ) {
+    
+}
+
