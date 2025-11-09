d82dd233a712e0a1d7711d07abcedca72bc63fa9
Nikolay <Kreicer0@gmail.com>
2025-11-09 18:22:14 +0300

    ДЗ Игра mole-game

--- js-features/mole-game/task.js
+++ js-features/mole-game/task.js
@@ -1,18 +1,50 @@
 
-
 const hitsWin = 10; // макс. количество попаданий
 const missesLose = 5; // макс. количество промахов
 const fieldHoles = 9; // поле 3х3
+let hole = document.querySelectorAll('.hole-game .hole');
 
 let hits = 0; // текущее количество попаданий
 let misses = 0; // текущее количество промахов
 
 const deadsMole = document.getElementById('dead');  // Попадания
 const missesDisplay = document.getElementById('lost'); // Промахи
-const gameContainer = document.getElementById('hole-game'); // Поле
-
-function getHole( index ) {
-    console.log(hits);
-}
 
 
+hole.forEach(function (item) {
+    item.addEventListener('click', () => {
+        if (item.classList.contains('hole_has-mole')) {
+            if (hits < hitsWin) {
+                hits += 1;
+                deadsMole.textContent = hits;
+            } else {
+                alert(`Количество попаданий: ${hits}. Вы победили!`);
+                endGame();
+            };
+        };
+    });
+    item.addEventListener('click', () => {
+        if (!item.classList.contains('hole_has-mole')) {
+            if (misses < missesLose) {
+                misses += 1;
+                missesDisplay.textContent = misses;
+            } else {
+                alert(`Количество промахов: ${misses}. Вы проиграли!`);
+                endGame();
+            };
+        };
+    });
+});
+
+function endGame() {
+    let gameOver = confirm('Продолжить игру?');
+    if(gameOver) {
+        hits = 0;
+        misses = 0;
+        deadsMole.textContent = hits;
+        missesDisplay.textContent = misses;
+    } else {
+        return;
+    }
+    
+};
