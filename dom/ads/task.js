b44f7924b7da977d9ae5fc1fefd4e084e1095550
Nikolay <Kreicer0@gmail.com>
2025-11-13 21:06:29 +0300

    ДЗ DOM

--- dom/ads/task.js
+++ dom/ads/task.js
@@ -4,14 +4,31 @@ button.textContent = 'Остановить';
 let container = document.querySelector('.card')
 container.appendChild(button)
 let rotator = document.querySelectorAll('.rotator__case');
+let newTimer;
+let newDelay = 1000;
 let index = 0;
 let rotatorActive = document.querySelector('.rotator__case_active');
+console.log(rotatorActive.dataset.speed)
+let timerSpeed = 1000;
 rotatorActive.classList.remove('rotator__case_active');
-let timer = setInterval(() => {
+
+function timerId() {
     rotator[index].classList.remove('rotator__case_active');
     index = Math.floor(Math.random() * 6);
+    timerSpeed = rotator[index].dataset.speed;
+    console.log('timerSpeed - ', timerSpeed);
+    rotator[index].style.color = rotator[index].dataset.color;
+    
     rotator[index].classList.value = 'rotator__case rotator__case_active';
-}, 1000);
+    changeDelay(timerSpeed);
+}
+let timer = setInterval(timerId, 1000);
+
+function changeDelay(newDelay) {
+    clearInterval(timer);
+    newTimer = setInterval(timerId, newDelay);
+}
+
 button.addEventListener('click', () => {
     clearInterval(timer);
 });
