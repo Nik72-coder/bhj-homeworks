7f28e07145e18a9ea5afa9922138d9cebaf26e14
Nikolay <Kreicer0@gmail.com>
2025-11-16 12:16:17 +0300

    ДЗ Объект события,TABS

--- event-object/tabs/task.js
+++ event-object/tabs/task.js
@@ -1,43 +1,17 @@
 
-
-let j = 0;
-
 let tab = document.querySelectorAll('.tab');
-
 let tab__content = document.querySelectorAll('.tab__content');
-console.log('tab - ', tab);
-console.log('tab__content - ', tab__content);
-let array = [];
-tab__content.forEach((item) => {
-        console.log(item);
-        array.push(item);
-    })
-    let t = 0;
-console.log('array - ', array);
 tab.forEach((item, index) => {
     item.addEventListener('click', () => {
         if (item.classList.value !== 'tab tab_active') {
             for (let i = 0; i < tab.length; i++) {
                 if (tab[i].classList.value === 'tab tab_active') {
-                    console.log(`tab(${[i]}).classList.value - `, tab[i].classList.value);
-                    tab[i].classList.remove('tab_active');
-                    t = tab[i].classList.value;
-                    console.log(`tab(${[i]}).classList.value - `, tab[i].classList.value);
+                    tab__content[i].classList.remove('tab__content_active');
+                    tab[i].classList.remove('tab_active');       
                 };
             };
-            console.log('t - ', t)
-            console.log('item - ', item);
             item.classList.add('tab_active');
-            console.log('item - ', item);
-            console.log('index - ', index);
-            console.log(`array(${[index]}).classList.value - `, array[index].classList.value);
-            array[index].classList.add('tab__content_active');
-            console.log(`array(${[index]}).classList.value - `, array[index].classList.value);
-            item.classList.value = array[index].classList.value;
-            console.log(`${item}.classList.value - `, item.classList.value);
-            console.log(`tab(${0}).classList.value - `, tab[0].classList.value);
+            tab__content[index].classList.add('tab__content_active');
         };
-        console.log('t - ', t);
     });
 });
-console.log('t - ', t);
