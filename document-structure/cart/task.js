51b6ea56f21f9b4704e145175197020d915c253e
Nikolay <Kreicer0@gmail.com>
2025-11-26 01:45:20 +0300

    ДЗ Изм-е структуры, корзина товаров

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,33 +1,36 @@
 
 let minus = document.getElementsByClassName('product__quantity-control_dec'); // кнопка уменьшения товара
 let productQuantityValue = document.getElementsByClassName('product__quantity-value'); // количество товара для заказа
+
 let plus = document.getElementsByClassName('product__quantity-control_inc'); // кнопка увеличения товара
 let productAdd = document.getElementsByClassName('product__add'); // кнопка добавления товара в корзину
-let product = document.getElementsByClassName('product'); // карточка товара
 
-let productTitle = document.getElementsByClassName('product__title'); // HTML-collection наименований товара
+
+let product = document.getElementsByClassName('product'); // HTMLCollection карточек товара
+let productTitle = document.getElementsByClassName('product__title'); // HTMLcollection наименований товара
+
+
 let cartProductCount = document.getElementsByClassName('cart__product-count');
 cartProductCount = cartProductCount[0];                  // индикатор количества товара в корзине
+
 let cartProductImage = document.getElementsByClassName('cart__product-image'); // картинка товара в корзине
 cartProductImage = cartProductImage[0];
 let productImage = document.getElementsByClassName('product__image'); // картинка товара для заказа
+
+
 let productId = [];                                            // массив артикулов товаров
 for (let i = 0; i < product.length; i++) {
     productId.push(product[i].dataset.id);
 }
 
-let cartProductArray = []; // массив артикулов товаров в корзине
 let cartProduct = document.getElementsByClassName('cart__product'); // добавленный в корзину товар
 cartProduct = cartProduct[0];
 let cartProducts = document.getElementsByClassName('cart__products'); // добавленный в корзину товар
 cartProducts = cartProducts[0];
-let cartProductObject = { name: 'товар', id: 1, src: '' }; // объект товара в корзине
+// let cartProductObject = { name: 'товар', id: 1, src: '', count: 1 }; // объект товара в корзине
 
-
-
-cartProductCount.textContent = 'Заказов нет';
+cartProduct.textContent = 'Корзина пуста';
 let cartProductObjectArray = []; // массив товаров(объектов) в корзине
-
 
 for (let i = 0; i < minus.length; i++) {
     minus[i].addEventListener('click', () => {
@@ -42,44 +45,77 @@ for (let i = 0; i < plus.length; i++) {
         productQuantityValue[i].textContent = Number(productQuantityValue[i].textContent) + 1;
     });
 };
-
-
 for (let i = 0; i < productAdd.length; i++) {
     productAdd[i].addEventListener('click', () => {
         cartProduct.remove();
-        makeObject(i);
-        makeDiv(i);
+        if (cartProductObjectArray.length === 0) {
+            makeObject(i);
+        } else {
+            let cartProductId = cartProductObjectArray.find((cartProductObject) => {
+                cartProductObject.id = productId[i];
+                return console.log('cartProductObject.id - ', cartProductObject.id);
+
+            })
+            if (cartProductId !== -1) {
+                makeObject(i);
+            } else {
+
+
+            }
+
+            // {
+            //     if (cartProductObjectArray[j].id.includes(productId[i])) {
+            //         console.log('Товар есть в корзине - ', cartProductObjectArray[j].id.includes(productId[i]));
+            //         cartProductObjectArray[j].count = productQuantityValue[i].textContent;
+            //         continue;
+            //     } else {
+            //         console.log('Товар есть в корзине - ', cartProductObjectArray[j].id.includes(productId[i]));
+            //         makeObject(i);
+
+
+            //     }
+            // }
+        }
+
+        // makeDiv(i);
     });
 };
 
 function makeObject(i) {
     let cartProductObject = {};
-    cartProductObject.id = product[i].getAttribute('data-id');
-    cartProductObject.name = product[i].textContent;
-    cartProductObject.src = productImage[i].getAttribute('src');
 
-    cartProductObjectArray.push(cartProductObject)
+    cartProductObject.id = productId[i];
+    cartProductObject.src = productImage[i].src;
+    cartProductObject.name = productTitle[i].textContent;
+    cartProductObject.count = productQuantityValue[i].textContent;
+    cartProductObjectArray.push(cartProductObject);
+
+
+    console.log('Корзина - ', cartProductObjectArray);
 }
 
-function makeDiv(i) {
-    let newDiv = document.createElement('div');
-    newDiv.className = 'cart__product';
-    newDiv.src = productImage[i].getAttribute('src');
-    newDiv.textContent = productTitle[i].textContent;
 
-    let newcountDiv = document.createElement('div');
-    newcountDiv.className = 'cart__product-count';
-    newcountDiv.textContent = productQuantityValue[i].textContent;
 
-    let newImg = document.createElement('img');
-    newImg.className = 'cart__product-image';
-    newImg.src = productImage[i].currentSrc;
 
-    if(cartProductCount.textContent !== productQuantityValue[i].textContent) {
-        cartProductCount.textContent = productQuantityValue[i].textContent;
-    };
+// function makeDiv(i) {
+//     let newDiv = document.createElement('div');
+//     newDiv.className = 'cart__product';
+//     newDiv.src = cartProductObjectArray[i].src;
+//     newDiv.textContent = cartProductObjectArray[i].name;
 
-    cartProducts.appendChild(newDiv);
-    newDiv.appendChild(newImg);
-    newDiv.appendChild(newcountDiv);
-}
+//     let newcountDiv = document.createElement('div');
+//     newcountDiv.className = 'cart__product-count';
+//     newcountDiv.textContent = cartProductObjectArray[i].count;
+
+//     let newImg = document.createElement('img');
+//     newImg.className = 'cart__product-image';
+//     newImg.src = productImage[i].currentSrc;
+
+//     if (cartProductCount.textContent !== cartProductObjectArray[i].count) {
+//         cartProductCount.textContent = productQuantityValue[i].textContent;
+//     };
+
+//     cartProducts.appendChild(newDiv);
+//     newDiv.appendChild(newImg);
+//     newDiv.appendChild(newcountDiv);
+// }
